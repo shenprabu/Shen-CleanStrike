4d35950d9a0cd07eef04e45face6e6d0fa8019af
ShenPrabu <aspraburaj197@gmail.com>
2019-08-23 10:45:44 +0530

    initial design - files

--- CleanStrike.java
+++ CleanStrike.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package cleanstrike;
 
 import java.util.ArrayList;
@@ -49,6 +45,7 @@ public class CleanStrike {
                 p.addScore(2);
                 p.addHistory(2);
                 b.reduceCoin(2);
+                break;
         }
     }
     
--- Player.java
+++ Player.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package cleanstrike;
 
 import java.util.ArrayList;
